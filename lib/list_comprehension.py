50426577c42200da7fc67bdb29c25e8b50253680
Michael Muchiri <muchirimichael2222@gmail.com>
2023-11-29 22:20:35 +0300

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [n for n in num_list if n % 2 == 0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list = [n + "!" for n in sentence_list]
+    return sentence_list
+
+#Case examples
+print(make_exclamation(["Yo", "Hello", "I'm a software developer"]))
+print(make_exclamation([]))
+print(return_evens([1,2,3,4,5,6]))
+print(return_evens([]))
